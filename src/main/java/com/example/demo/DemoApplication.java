802039ee0408d0e289c766976d46f8d0b099294c
nologicurmom <108929982+nologicurmom@users.noreply.github.com>
2023-01-24 14:08:39 +0300

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,16 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@EnableScheduling
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.demo.controllerAPI")
 @ComponentScan(basePackages = "com.example.demo.controllerVIEW")
